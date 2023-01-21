f906ea16a3519647d0b038ea04de5b765cc5a2a4
Andrei Hutsau <mwplt@mail.ru>
2023-01-22 00:21:50 +0300

    finish1

--- src/AppWithRedux.tsx
+++ src/AppWithRedux.tsx
@@ -29,8 +29,6 @@ export type TasksStateType = {
 
 
 function AppWithRedux() {
-    let todolistId1 = v1();
-    let todolistId2 = v1();
 
     const todolists = useSelector<AppRootStateType, Array<TodolistType>>(state => state.todolists)
     const tasks = useSelector<AppRootStateType, TasksStateType>(state => state.tasks)
